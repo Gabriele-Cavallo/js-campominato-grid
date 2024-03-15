f498d47805569a16709c8ace7ebe95eef15bd152
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-15 14:27:49 +0100

    feat: create grid 10x10

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@
 // - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 // - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 // - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+// 1 - Creo l'elemento base del DOM
+let square = document.createElement('div');
+square.classList.add('square');
+square.innerHTML = `<span>1</span>`;
+// 2 - Genero la tabella da 10 x 10 numerata proggressivamente inserendo l'elemento del DOM creato nel ciclo for
+let userGrid = document.querySelector('#grid');
+for (let i = 0; i < 100; i++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    square.innerHTML = `<span>${i + 1}</span>`;
+    userGrid.append(square);
+}
+// 3 - Creo una funzione al click per colorare la casella di azzurro ed stampa in console di un messaggio con il numero
+// 4 - Inserisco la funzione nel ciclo del programma
